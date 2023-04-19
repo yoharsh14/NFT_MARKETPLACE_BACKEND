f7c8f0e62f98813f347d39d08e7e0dd1914d8704
yoharsh14 <harshdambhareh53@gmail.com>
2023-04-19 14:48:48 +0530

    buy Item function created

--- contracts/NftMarketPlace.sol
+++ contracts/NftMarketPlace.sol
@@ -5,16 +5,24 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 error NftMarketplace_PriceMustBeAboveZero();
 error NftMarketplace_NotApprovedForMarketplace();
 error NftMarketplace_NotOwner();
+error NftMarketplace_NotListed(address nftAddress, uint256 tokenId);
 error NftMarketplace_AlreadyListed(address nftAddress, uint256 tokenId);
+error NftMarketplace_PriceNotMet(address nftAddress, uint256 tokenId, uint256 price);
 
 contract NftMarketplace {
     struct Listing {
         uint256 price;
         address seller;
     }
+    // Events
     event ItemList(address indexed seller, address nftAddress, uint256 tokenId, uint256 price);
+    event ItemBought(address indexed buyer, address nftAddress, uint256 tokenId, uint256 price);
+
     //NFT contract address -> NFT TokenId -> Listing
     mapping(address => mapping(uint256 => Listing)) private s_listings;
+
+    //Seller address -> Amount earned
+    mapping(address => uint256) private s_proceeds;
 
     ////////////////////
     //    Modifiers   //
@@ -44,17 +52,25 @@ contract NftMarketplace {
         _;
     }
 
+    modifier isListed(address nftAddress, uint256 tokenId) {
+        Listing memory listing = s_listings[nftAddress][tokenId];
+        if (listing.price <= 0) {
+            revert NftMarketplace_NotListed(nftAddress, tokenId);
+        }
+        _;
+    }
+
     ///////////////////
     // Main Function //
     ///////////////////
 
     /** @notice Method for lisitng your NFT on the marketplace
-    * @param nftAddress: Address of the NFT
-    * @param tokenId: The Token ID of the NFT
-    * @param price: sale price of the listed NFT
-    * @dev Technically, we could have the contract be the escrow for the NFTs
-    * but this way people can still hold their NFTs when listed.
-    */
+     * @param nftAddress: Address of the NFT
+     * @param tokenId: The Token ID of the NFT
+     * @param price: sale price of the listed NFT
+     * @dev Technically, we could have the contract be the escrow for the NFTs
+     * but this way people can still hold their NFTs when listed.
+     */
     function listItem(
         address nftAddress,
         uint256 tokenId,
@@ -73,7 +89,29 @@ contract NftMarketplace {
         emit ItemList(msg.sender, nftAddress, tokenId, price);
     }
 
- 
+    // I can also use crypto token for buying the NFT's,
+    // by checking the price with the help of chainlink priceFeed
+
+    function buyItem(
+        address nftAddress,
+        uint256 tokenId,
+        uint256 price
+    ) external payable isListed(nftAddress, tokenId) {
+        Listing memory listedItem = s_listings[nftAddress][tokenId];
+        if (msg.value < listedItem.price) {
+            revert NftMarketplace_PriceNotMet(nftAddress, tokenId, price);
+        }
+        // We don't just send seller the money ...
+        // https://fravoll.github.io/solidity-patterns/pull_over_push.html
+
+        //sending the money to the user
+        // Have them withdraw the money
+        s_proceeds[listedItem.seller] = s_proceeds[listedItem.seller] + msg.value;
+        delete (s_listings[nftAddress][tokenId]);
+        IERC721(nftAddress).safeTransferFrom(listedItem.seller, msg.sender, tokenId);
+        //check to ensure
+        emit ItemBought(msg.sender, nftAddress, tokenId, listedItem.price);
+    }
 }
 
 // 1. `listItem`: List NFTs on the marketplace
