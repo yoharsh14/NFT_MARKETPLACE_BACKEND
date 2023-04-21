a60cbd49b64d31c466b9437006eafefcd20bf813
yoharsh14 <harshdambhareh53@gmail.com>
2023-04-21 15:01:12 +0530

    Contract Completed

--- contracts/NftMarketPlace.sol
+++ contracts/NftMarketPlace.sol
@@ -2,21 +2,25 @@
 
 pragma solidity ^0.8.7;
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 error NftMarketplace_PriceMustBeAboveZero();
 error NftMarketplace_NotApprovedForMarketplace();
 error NftMarketplace_NotOwner();
 error NftMarketplace_NotListed(address nftAddress, uint256 tokenId);
 error NftMarketplace_AlreadyListed(address nftAddress, uint256 tokenId);
 error NftMarketplace_PriceNotMet(address nftAddress, uint256 tokenId, uint256 price);
+error NftMarketplace_NoProceeds();
+error NftMarketplace_TransferFailed();
 
-contract NftMarketplace {
+contract NftMarketplace is ReentrancyGuard {
     struct Listing {
         uint256 price;
         address seller;
     }
     // Events
-    event ItemList(address indexed seller, address nftAddress, uint256 tokenId, uint256 price);
+    event ItemListed(address indexed seller, address nftAddress, uint256 tokenId, uint256 price);
     event ItemBought(address indexed buyer, address nftAddress, uint256 tokenId, uint256 price);
+    event ItemCanceled(address indexed seller, address nftAddress, uint256 tokenId);
 
     //NFT contract address -> NFT TokenId -> Listing
     mapping(address => mapping(uint256 => Listing)) private s_listings;
@@ -86,7 +90,7 @@ contract NftMarketplace {
             revert NftMarketplace_NotApprovedForMarketplace();
         }
         s_listings[nftAddress][tokenId] = Listing(price, msg.sender);
-        emit ItemList(msg.sender, nftAddress, tokenId, price);
+        emit ItemListed(msg.sender, nftAddress, tokenId, price);
     }
 
     // I can also use crypto token for buying the NFT's,
@@ -96,7 +100,7 @@ contract NftMarketplace {
         address nftAddress,
         uint256 tokenId,
         uint256 price
-    ) external payable isListed(nftAddress, tokenId) {
+    ) external payable nonReentrant isListed(nftAddress, tokenId) {
         Listing memory listedItem = s_listings[nftAddress][tokenId];
         if (msg.value < listedItem.price) {
             revert NftMarketplace_PriceNotMet(nftAddress, tokenId, price);
@@ -111,6 +115,47 @@ contract NftMarketplace {
         IERC721(nftAddress).safeTransferFrom(listedItem.seller, msg.sender, tokenId);
         //check to ensure
         emit ItemBought(msg.sender, nftAddress, tokenId, listedItem.price);
+    }
+
+    function cancleListing(
+        address nftAddress,
+        uint256 tokenId
+    ) external isOwner(nftAddress, tokenId, msg.sender) isListed(nftAddress, tokenId) {
+        delete (s_listings[nftAddress][tokenId]);
+        emit ItemCanceled(msg.sender, nftAddress, tokenId);
+    }
+
+    function updateListing(
+        address nftAddress,
+        uint256 tokenId,
+        uint256 newPrice
+    ) external isListed(nftAddress, tokenId) isOwner(nftAddress, tokenId, msg.sender) {
+        s_listings[nftAddress][tokenId].price = newPrice;
+        emit ItemListed(msg.sender, nftAddress, tokenId, newPrice);
+    }
+
+    function withdrawProceeds() external {
+        uint256 proceeds = s_proceeds[msg.sender];
+        if (proceeds <= 0) revert NftMarketplace_NoProceeds();
+        s_proceeds[msg.sender] = 0;
+        (bool success, ) = payable(msg.sender).call{value: proceeds}("");
+        if (!success) {
+            revert NftMarketplace_TransferFailed();
+        }
+    }
+
+    ///////////////////////
+    // getter Functions ///
+    ///////////////////////
+    function getlisting(
+        address nftAddress,
+        uint256 tokenId
+    ) external view returns (Listing memory) {
+        return s_listings[nftAddress][tokenId];
+    }
+
+    function getProceeds(address seller) external view returns (uint256) {
+        return s_proceeds[seller];
     }
 }
 
--- contracts/sublesson/reentrancyAttack.sol
+++ contracts/sublesson/reentrancyAttack.sol
@@ -9,12 +9,14 @@ contract ReentrantVulnerable {
         balances[msg.sender] += msg.value;
     }
 
-    function withdraw() public {
+// easy way
+// mutex lock using openZeppeline
+    function withdraw() payable public {
         uint256 bal = balances[msg.sender];
         require(bal > 0);
+        balances[msg.sender] = 0;
         (bool sent, ) = msg.sender.call{value: bal}("");
         require(sent, "Failed to send Ether");
-        balances[msg.sender] = 0;
     }
 
     function getBalance() public view returns (uint256) {
@@ -44,3 +46,18 @@ contract Attack {
         return address(this).balance;
     }
 }
+
+//1. Let say you are Alice and you have deposited 100ETH in the contract
+//2. Now Bob want to attack the contract then he deploy the Attack contract by passing the address of the
+// contract where Alice's ETH is stored.
+//3. Now Bob will use the attack function.
+// FLOW OF ATTACK FUNCTION
+// -> Calls the deposit function and deposit 1ETH because the balance should be 1ETH so that the withdraw function will work.
+// -> After depositing 1ETH the attack function will call withdraw function.
+// -> In the First Contract Withdraw function first check if the deposit of the caller is more than 0ETH.
+// -> then withdraw function runs the msg.sender.call function which is the worst problem.
+// -> this msg.sender.call is calling the contract from where is hasbeen called then in the attack contract that call be be handled by
+//    the fallback function, and that fallback function again call the withdraw function then this recursion goes on and on which causes 
+// loosing of all the money from the contarct. and balance of attacked contract be become zero.
+
+// #######Best practice is to first update the balance to zero then transfer the funds############//
